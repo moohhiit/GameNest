aa986788132dc8502960789fdc46b37b7c0061c5
moohhiit <itzmohitsharma20@gmail.com>
2025-07-19 14:57:00 +0530

    Complete Local SPS Screen

--- components/SPS/Local-SPS-Board.tsx
+++ components/SPS/Local-SPS-Board.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { View, StyleSheet, Text, Dimensions, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, Dimensions, Image, TouchableOpacity, Modal } from 'react-native';
 
 import AvatarRing, { AvatarRingRef } from './Flip-Ring-Animation';
 import { getSPSGameResult } from '../../utils/SPSLogic';
+import { useNavigation } from '@react-navigation/native';
 
 type PlayerDetail = {
   player_name: string,
@@ -30,6 +31,8 @@ export default function LocalSPSBoard({
   const [winRoundPlayer1, setwinRoundPlayer1] = useState(0)
   const [winRoundPlayer2, setwinRoundPlayer2] = useState(0)
   const [toggleturn, settoggelTurn] = useState(true)
+  const [visible, setVisible] = useState(false)
+
 
   const playerImages = {
     Questionmark: require('../../assets/images/Questionmark.png'),
@@ -39,19 +42,35 @@ export default function LocalSPSBoard({
   };
   const ringRef1 = useRef<AvatarRingRef>(null)
   const ringRef2 = useRef<AvatarRingRef>(null)
+  const modalringRef1 = useRef<AvatarRingRef>(null)
+  const modalringRef2 = useRef<AvatarRingRef>(null)
 
-  const showResult = ()=>{
-    const result = getSPSGameResult(optionPlayer1 , optionPlayer2)
-    if(result){
-      ringRef1.current?.triggerWin()
-      setwinRoundPlayer1(winRoundPlayer1 + 1)
-      setCompletedRound(CompletedRound + 1)
-    }
-    else{
-      ringRef2.current?.triggerWin()
-      setwinRoundPlayer2(winRoundPlayer2 + 1)
-      setCompletedRound(CompletedRound + 1)
-    }
+  const Navigation = useNavigation()
+
+  const tryAgain = () => {
+    setOptionPlayer1('Questionmark')
+    setOptionPlayer2('Questionmark')
+    setVisible(false)
+  }
+
+  const showResult = () => {
+    setVisible(true)
+    setTimeout(() => {
+      const result = getSPSGameResult(optionPlayer1, optionPlayer2)
+      modalringRef1.current?.flipRing()
+      modalringRef2.current?.flipRing()
+      if (result) {
+        modalringRef1.current?.triggerWin()
+        setwinRoundPlayer1(winRoundPlayer1 + 1)
+        setCompletedRound(CompletedRound + 1)
+      }
+      else {
+        modalringRef2.current?.triggerWin()
+        setwinRoundPlayer2(winRoundPlayer2 + 1)
+        setCompletedRound(CompletedRound + 1)
+      }
+    }, 1000);
+
   }
 
   useEffect(() => {
@@ -60,6 +79,50 @@ export default function LocalSPSBoard({
 
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={() => setVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContainer}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }} >
+              <View>
+                <AvatarRing ref={modalringRef1} frontImage={require('../../assets/images/Questionmark.png')} backImage={playerImages[optionPlayer1]} AVATAR_SIZE={120} RING_SIZE={160} RADIUS={75} STROKE_WIDTH={5} TIME={10} BORDER_RADIUS={50} />
+                <Text style={{ textAlign: 'center', fontSize: 20 }} >
+                  {LocalPlayerDetail.player_name}
+                </Text>
+              </View>
+              <View style={{ justifyContent: 'center', alignContent: 'center' }} >
+                <AvatarRing ref={modalringRef2} frontImage={require('../../assets/images/Questionmark.png')} backImage={playerImages[optionPlayer2]} AVATAR_SIZE={120} RING_SIZE={160} RADIUS={75} STROKE_WIDTH={5} TIME={10} BORDER_RADIUS={50} />
+                <Text style={{ textAlign: 'center', fontSize: 20 }} >
+                  {PlyerTwoDetail.player_name}
+                </Text>
+              </View>
+
+            </View>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', padding: 20 }} >
+              <TouchableOpacity style={{ backgroundColor: '#53ff28ff', padding: 10, borderRadius: 10 }} 
+              onPress={()=>tryAgain()}
+              >
+                <Text style={{ fontSize: 20, fontWeight: '700' }} >
+                  Try Again
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{ backgroundColor: '#ff0000ff', padding: 10, borderRadius: 10 }}
+                onPress={()=>Navigation.navigate('HomeScreen' as never)}
+              >
+                <Text style={{ fontSize: 20, fontWeight: '700' }} >
+                  Go To Home
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </View>
+      </Modal>
       <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end', gap: 20 }} >
         {/* Round Indicator */}
         <View style={{ backgroundColor: '#222831', width: '40%', borderBottomLeftRadius: 20, height: "50%", borderBottomRightRadius: 20, justifyContent: 'center', alignItems: 'center' }} >
@@ -183,7 +246,7 @@ export default function LocalSPSBoard({
                 }}
               >
                 <Text style={{ fontSize: 25, color: 'white' }} >
-                  Conferm
+                  Confirm
                 </Text>
               </TouchableOpacity> : null
           }
@@ -229,5 +292,45 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 20,
+  },
+  openButton: {
+    backgroundColor: '#2196F3',
+    padding: 12,
+    borderRadius: 8,
+    margin: 20,
+  },
+  openText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'flex-end',
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    paddingVertical: 30,
+    position: 'relative',
+
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 80,
+    textAlign: 'center',
+  },
+  confirmButton: {
+    backgroundColor: '#4CAF50',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    bottom: 20,
+    alignSelf: 'center',
+  },
+  confirmText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
